debe7057a58901a2c4ee6d57aee22cb76051a47b
Alaricdragon <alaric.meservy@gmail.com>
2024-03-12 12:59:17 -0700

    init

---
+++ jars/src/Lobster_Combatant/data/scripts/LobsterCombatant_startup.java
@@ -0,0 +1,17 @@
+package Lobster_Combatant.data.scripts;
+import com.fs.starfarer.api.BaseModPlugin;
+public class LobsterCombatant_startup extends BaseModPlugin {
+    @Override
+    public void onApplicationLoad() {
+    }
+
+    @Override
+    public void onGameLoad(boolean newGame) {
+        super.onGameLoad(newGame);
+    }
+
+    @Override
+    public void beforeGameSave() {
+        super.beforeGameSave();
+    }
+}
