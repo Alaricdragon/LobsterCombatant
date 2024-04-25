8c9daf9724a3ed9239a237de3f373c8a6b3a904d
Alaricdragon <alaric.meservy@gmail.com>
2024-04-25 13:01:05 -0700

    basic commoditys done maybe

--- jars/src/Lobster_Combatant/data/scripts/LobsterCombatant_startup.java
+++ jars/src/Lobster_Combatant/data/scripts/LobsterCombatant_startup.java
@@ -1,6 +1,7 @@
 package Lobster_Combatant.data.scripts;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import data.scripts.crewReplacer_Main;
 
 public class LobsterCombatant_startup extends BaseModPlugin {
     @Override
@@ -8,5 +9,14 @@ public class LobsterCombatant_startup extends BaseModPlugin {
         super.onGameLoad(newGame);
         Global.getSector().getListenerManager().addListener(new LobsterCombatant_CommodityTooltipModifier(),true);
         Global.getSector().getEconomy().addUpdateListener(new LobsterCombatant_BaseCampaignEventListener());
+    }
+
+    @Override
+    public void onApplicationLoad() {
+        if (Global.getSettings().getModManager().isModEnabled("aaacrew_replacer")) {
+            crewReplacer_Main.getCrewSet("marines").addNewCrew("LobsterCombatant_CombatLobster",1.125f,1.5f,10,0);
+            crewReplacer_Main.getJob("raiding_marines").addNewCrew("LobsterCombatant_CombatLobster",1.25f,2,10,1);
+            crewReplacer_Main.getJob("Mission_hijack_marines").addBlackListCrew("LobsterCombatant_CombatLobster",1);
+        }
     }
 }
